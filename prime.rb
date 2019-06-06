678209091641224fb13b04f9d593a03f378bd52f
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 22:28:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime? (number)
+
+  if number <= 0 || number == 1 
+    return false
+  end
+
+  if number == 2
+    return true
+  end
+
+  for i in 2..(number - 1) do
+    if number % i == 0
+      return false
+    end
+  end
+
+  return true
+end
